3b2e34e988c2b78060208fef0e12422ec470012d
nbalabis <88114907+nbalabis@users.noreply.github.com>
2021-10-13 11:59:07 -0700

    Generate javadoc for class Outsourced

--- src/model/Outsourced.java
+++ src/model/Outsourced.java
@@ -1,17 +1,46 @@
 package model;
 
+/**
+ * Model for Outsourced Part.
+ *
+ * @author Nicholas Balabis
+ */
 public class Outsourced extends Part{
+    /**
+     * Company Name of the part.
+     */
     private String companyName;
 
+    /**
+     * Constructor for a new Outsourced part.
+     *
+     * @param id Id of the part.
+     * @param name Name of the part.
+     * @param price Price of the part.
+     * @param stock Inventory of the part.
+     * @param min Minimum inventory required of the part.
+     * @param max Maximum inventory allowed of the part.
+     * @param companyName Company name of the part.
+     */
     public Outsourced(int id, String name, double price, int stock, int min, int max, String companyName) {
         super(id, name, price, stock, min, max);
         this.companyName = companyName;
     }
 
+    /**
+     * Company name setter.
+     *
+     * @param companyName Company name of the part.
+     */
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
     }
 
+    /**
+     * Company name getter.
+     *
+     * @return Company name of the part.
+     */
     public String getCompanyName() {
         return companyName;
     }
