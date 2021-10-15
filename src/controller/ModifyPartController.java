facdfc62061c7f0c1962b329b33de0f6d086edbf
nbalabis <88114907+nbalabis@users.noreply.github.com>
2021-10-15 10:04:41 -0700

    Generate javadoc for ModifyPartController

--- src/controller/ModifyPartController.java
+++ src/controller/ModifyPartController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -16,26 +17,69 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * Controller logic for Modify Part page.
+ *
+ * @author Nicholas Balabis
+ */
 public class ModifyPartController implements Initializable {
+    @FXML
     public ToggleGroup partTG;
+
+    @FXML
     public RadioButton partInHouseRBtn;
+
+    @FXML
     public RadioButton partOutsourcedRBtn;
+
+    @FXML
     public TextField partIdTxt;
+
+    @FXML
     public TextField partNameTxt;
+
+    @FXML
     public TextField partInvTxt;
+
+    @FXML
     public TextField partPriceTxt;
+
+    @FXML
     public TextField partMaxTxt;
+
+    @FXML
     public TextField partMinTxt;
+
+    @FXML
     public TextField partTypeTxt;
+
+    @FXML
     public Label partType;
+
+    @FXML
     public Label errorMessage;
+
+    @FXML
     private static Part selectedPart = null;
+
+    @FXML
     public Button homeButton;
 
+    /**
+     * Grabs selected part from Main screen.
+     *
+     * @param part Selected part.
+     */
     public static void getSelectedPart(Part part) {
         selectedPart = part;
     }
 
+    /**
+     * Initializes controller, fills text fields with starting values, and selects radio button.
+     *
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         partIdTxt.setText(String.valueOf(selectedPart.getId()));
@@ -57,27 +101,34 @@ public class ModifyPartController implements Initializable {
         }
     }
 
-    public void toMain(ActionEvent actionEvent) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
-        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root, 760, 320);
-        stage.setTitle("Main Form");
-        stage.setScene(scene);
-        stage.show();
-    }
-
+    /**
+     * Sets label to "Machine ID"
+     *
+     * @param actionEvent In-House radio button clicked.
+     */
     public void onInHouse(ActionEvent actionEvent) {
         partType.setText("Machine ID");
     }
 
+    /**
+     * Sets label to "Company Name"
+     *
+     * @param actionEvent Outsourced radio button clicked.
+     */
     public void onOutsourced(ActionEvent actionEvent) {
         partType.setText("Company Name");
     }
 
+    /**
+     * Creates a new part and replaces old part.
+     *
+     * @param actionEvent Save button clicked.
+     * @throws IOException Throws IO Exception.
+     */
     public void savePart(ActionEvent actionEvent) throws IOException {
         int id = selectedPart.getId();
         String name = partNameTxt.getText();
-        Double price = Double.parseDouble(partPriceTxt.getText());
+        double price = Double.parseDouble(partPriceTxt.getText());
         int stock = Integer.parseInt(partInvTxt.getText());
         int min = Integer.parseInt(partMinTxt.getText());
         int max = Integer.parseInt(partMaxTxt.getText());
@@ -93,5 +144,21 @@ public class ModifyPartController implements Initializable {
         }
         Inventory.deletePart(selectedPart);
         homeButton.fireEvent(new ActionEvent());
+    }
+
+    /**
+     * Returns to main screen.
+     *
+     * @param actionEvent Cancel button clicked.
+     * @throws IOException Throws IO Exception.
+     */
+    //FIXME: can I change this to notnull?
+    public void toMain(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 760, 320);
+        stage.setTitle("Main Form");
+        stage.setScene(scene);
+        stage.show();
     }
 }
